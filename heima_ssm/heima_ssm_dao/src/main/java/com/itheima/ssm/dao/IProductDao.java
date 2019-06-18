6f71720ed97a8c1bf8121abab0995bd264781c5f
sharon <1964609481@qq.com>
2019-06-18 23:43:31 +0800

    day01 2019.6.18 23.43

---
+++ heima_ssm/heima_ssm_dao/src/main/java/com/itheima/ssm/dao/IProductDao.java
@@ -0,0 +1,11 @@
+package com.itheima.ssm.dao;
+
+import com.itheima.ssm.domain.Product;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+public interface IProductDao {
+    @Select("select * from product")
+    public List<Product> findAll();
+}
---
+++ heima_ssm/heima_ssm_domain/src/main/java/com/itheima/ssm/domain/Product.java
@@ -0,0 +1,99 @@
+package com.itheima.ssm.domain;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+public class Product {
+    private String id;
+    private String productNum;
+    private String productName;
+    private String cityName;//出发城市
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
+    private Date departureTime;//出发时间
+    private String departureTimeStr;
+    private double productPrice;
+    private String productDesc;
+    private Integer productStatus;
+    private String productStatusStr;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getProductNum() {
+        return productNum;
+    }
+
+    public void setProductNum(String productNum) {
+        this.productNum = productNum;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public Date getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(Date departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public String getDepartureTimeStr() {
+        return departureTimeStr;
+    }
+
+    public void setDepartureTimeStr(String departureTimeStr) {
+        this.departureTimeStr = departureTimeStr;
+    }
+
+    public double getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(double productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public String getProductDesc() {
+        return productDesc;
+    }
+
+    public void setProductDesc(String productDesc) {
+        this.productDesc = productDesc;
+    }
+
+    public Integer getProductStatus() {
+        return productStatus;
+    }
+
+    public void setProductStatus(Integer productStatus) {
+        this.productStatus = productStatus;
+    }
+
+    public String getProductStatusStr() {
+        return productStatusStr;
+    }
+
+    public void setProductStatusStr(String productStatusStr) {
+        this.productStatusStr = productStatusStr;
+    }
+}
